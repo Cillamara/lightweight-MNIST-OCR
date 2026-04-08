875219bd83510cbaccc98d3870c90eb64553a6d0
Balor-Brennan <bbrennan2301@gmail.com>
2026-04-08 21:24:24 +0000

    Add project source files

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ float func(float arg1, float arg2) {
     return arg1 + arg2;
 }
 
-PYBIND11_MODULE(module_name, handle){
-    handle.doc() = "example";
+PYBIND11_MODULE(mnistocr, handle){
+    handle.doc() = "This is a MNST OCR module";
     handle.def("test_func", &func);
 }
